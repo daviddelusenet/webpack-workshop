4923ef1ab87b5e4d8413f8b7a4cdaa37e44f26a8
David de Lusenet <lusenet@momkai.com>
2017-08-17 16:01:41 +0200

    Added example-11

---
+++ example-11/src/javascript/App.js
@@ -0,0 +1,30 @@
+// Import the styling
+import './../scss/App.scss';
+
+// Import the ComponentLoader
+import ComponentLoader      from 'component-loader-js';
+
+// Import the components
+import Bar                  from 'Bar/Bar';
+import Foo                  from 'Foo/Foo';
+
+// Scan for components on page load
+document.addEventListener('DOMContentLoaded', () => {
+
+  const componentLoader = new ComponentLoader({
+    Bar,
+    Foo
+  });
+
+  // Call scan() to instantiate any components found in the DOM
+  componentLoader.scan();
+
+  if (module.hot) {
+    module.hot.accept(function(err) {
+      if (err) {
+        console.error(err);
+      }
+    })
+  }
+
+});
