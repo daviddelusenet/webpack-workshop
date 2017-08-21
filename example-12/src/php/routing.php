4ea068a258537e63ba13c20c43608e343555ce41
David de Lusenet <lusenet@momkai.com>
2017-08-21 16:48:31 +0200

    Updated routing

--- example-12/src/php/routing.php
+++ example-12/src/php/routing.php
@@ -1,3 +1,5 @@
 <?php
 
 $app->get('/', 'Boilerplate\\Controllers\\HomeController::viewHome');
+$app->get('/component-a', 'Boilerplate\\Controllers\\ComponentAController::viewComponentA');
+$app->get('/component-b', 'Boilerplate\\Controllers\\ComponentBController::viewComponentB');
