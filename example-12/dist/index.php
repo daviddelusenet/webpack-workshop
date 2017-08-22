1d79476e5f5b68cab03ebd3c31b4f173c08874d1
David de Lusenet <lusenet@momkai.com>
2017-08-22 17:00:50 +0200

    Removed example-12

--- example-12/dist/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-// require needed files
-require_once __DIR__.'/../src/php/app.php';
-require_once __DIR__.'/../src/php/routing.php';
-
-// start application
-$app->run();
--- example-12/src/php/routing.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$app->get('/', 'Boilerplate\\Controllers\\HomeController::viewHome');
-$app->get('/component-a', 'Boilerplate\\Controllers\\ComponentAController::viewComponentA');
-$app->get('/component-b', 'Boilerplate\\Controllers\\ComponentBController::viewComponentB');
